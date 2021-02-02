96a552e3184ea081731ff069213ba9432ea78fe2
ALFONSO BALLESTEROS <alfonsoballesterosortiz@gmail.com>
2021-02-03 00:30:37 +0100

    Ejercicio de condicionales

---
+++ Clase14/Condiconales.py
@@ -0,0 +1,16 @@
+# edad = input ("Escribe tu edad: ")
+# edad = int (edad)
+# if edad > 17:    
+#     print ("Eres MAYOR de edad.")
+# else :
+#     print ("Eres MENOR de edad.")
+
+numero = input ("Escribe un número:  ")
+numero = int (numero)
+
+if numero > 5:   
+    print ('Es mayor a CINCO')
+elif numero == 5:   
+    print ("Es igual a CINCO")
+else:    
+    print ("Es menor a CINCO")
